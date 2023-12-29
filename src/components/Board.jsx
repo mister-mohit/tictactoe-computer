2feb7416005956c144cfae4e07c68c8008331899
Mohit <mohitx618@gmail.com>
2023-12-29 14:47:04 +0530

    Fist Commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import Square from "./Square";
+
+const Board = () => {
+  const [square, setSquare] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleClick = (clickedPosition) => {
+    if (square[clickedPosition]) {
+      return;
+    }
+
+    setSquare((squareArr) => {
+      return squareArr.map((squareVal, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? "X" : "0";
+        }
+
+        return squareVal;
+      });
+    });
+
+    setIsXNext((prevVAl) => !prevVAl);
+  };
+
+  const renderSquare = (position) => (
+    <Square value={square[position]} onClick={() => handleClick(position)} />
+  );
+
+  return (
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const Square = ({ value, onClick }) => {
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
